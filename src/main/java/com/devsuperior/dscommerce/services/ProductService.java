d61b8f93d499f2e8fef9f246be5f1e1a86b30bf3
zanutt <matszg@live.com>
2025-02-16 14:17:24 -0300

    feat: delete product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -45,6 +45,11 @@ public class ProductService {
 		entity = repository.save(entity);				
 		return new ProductDTO(entity);
 	}
+
+	@Transactional
+	public void delete(Long id) {		
+		repository.deleteById(id);
+	}
 		
 	private void copyDtoToEntity(ProductDTO dto, Product entity) {		
 		entity.setName(dto.getName());
