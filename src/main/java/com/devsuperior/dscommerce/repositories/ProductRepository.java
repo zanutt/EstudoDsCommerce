97a5ab523943378d4c86b4a490c820c7201c45de
zanutt <matszg@live.com>
2025-03-19 17:22:14 -0300

    feat: add find by request param name

--- src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.devsuperior.dscommerce.repositories;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dscommerce.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
+
+    @Query("SELECT obj FROM Product obj " 
+        +   "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%',:name, '%')) ")
+    Page<Product> searchByName(String name, org.springframework.data.domain.Pageable pageable);
 
 }
